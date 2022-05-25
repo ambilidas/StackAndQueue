2fe32fa042a8963e4437c9a8605805f19724e056
Ambili das v s <ambili.kan@gmail.com>
2022-05-25 14:53:49 +0530

    Created stack and inserted given elements

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@ namespace StackAndQueue
         public static void Main(string[] args)
         {
             Console.WriteLine("\t\tStack and Queues");
+            Stack stackobj = new Stack();
+            stackobj.Push(70);
+            stackobj.Push(30);
+            stackobj.Push(56);
+            stackobj.Display();
+            Console.ReadKey();
+
         }
     }
 }
---
+++ Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class Stack
+    {
+        Node top;
+        public Stack()
+        {
+            top = null;
+        }
+        public void Push(int data)
+        {
+            Node newnode=new Node(data);
+            newnode.next = top;
+            top = newnode;
+        }
+        public void Display()
+        {
+            if(top == null)
+            {
+                Console.WriteLine("Stack is empty!!");
+            }
+            Node temp = top;
+            while(temp != null)
+            {
+                Console.Write(temp.data + "->");
+                temp = temp.next;
+            }
+        }
+    }
+}
