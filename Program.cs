c288e3f5b5c574deaa059614c5d8013e9f90e59a
Ambili das v s <ambili.kan@gmail.com>
2022-05-25 14:34:39 +0530

    Program for creating stack and queue is created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tStack and Queues");
+        }
+    }
+}
